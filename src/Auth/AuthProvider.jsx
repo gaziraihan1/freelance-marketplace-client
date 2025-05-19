ccbae81660413c9996307adc4b3101defd68f480
gaziraihan1 <gaziraihan505@gmail.com>
2025-05-19 21:23:53 +0600

    auth context setup completed successfull

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,4 +1,6 @@
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import React, { createContext, useEffect } from 'react';
+import { auth } from '../firebase/firebase.init';
 
 
 
@@ -7,8 +9,41 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
-    const authValue = {
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
 
+    const signinUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+
+    const updateUser = (updateData) => {
+    return updateProfile(auth.currentUser, updateData);
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  }
+
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        console.log(currentUser)
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+    const authValue = {
+        createUser,
+        signinUser,
+        logOut,
+        updateUser,
+        resetPassword
     }
 
     return <AuthContext value={authValue}>
